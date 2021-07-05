f31b1273ebc7d8eca3d578081c571c64143fb46e
mateoU1297 <castro.mateo@correounivalle.edu.co>
2021-07-05 16:27:15 -0500

    se termina los pipes no-comunes

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -19,12 +20,38 @@ export class NoComunesComponent {
   //i18nPlural
   clientes: string[] = ['Maria', 'Pedro', 'Pablo'];
 
-  //keyValue
+  //keyValuePipe
   persona = {
     nombre: 'Mateo',
     edad: 24,
     direccion: 'Cali, Colombia'
   }
+
+  //JsonPipe
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: true
+    },
+    {
+      nombre: 'Robin',
+      vuela: false
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false
+    }
+  ]
+
+  //AsyncPipe
+  miObservable = interval(1000);
+
+  valorPromesa = new Promise ((resolve, reject) =>{
+
+    setTimeout(() => {
+      resolve('Tenemos data de promesa');
+    }, 3500);
+  });
 
   clientesMapa = {
     '=0': 'no tenemos ningún cliente esperando.',
